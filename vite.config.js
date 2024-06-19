4951b06fdb258148bf145fc8ab893b0c4099529b
Fabian <olmosfabian@hotmail.com>
2024-06-19 11:18:23 -0300

    Configurar la ruta base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Surge-/', // Reemplaza 'Surge-' con el nombre de tu repositorio
+});
