7f2801a8a34c93647b1d559d8794cc04bb5d366e
Fabian <olmosfabian@hotmail.com>
2024-07-20 11:56:33 -0300

    Corregir ruta de la imagen de WhatsApp y actualizar importaciones

--- src/Pages/WhatsappButton.jsx
+++ src/Pages/WhatsappButton.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import './FiveAccordions'; // Importa los estilos para el botó
+import whatsappIcon from '../assets/whatsapp-icon.png'; // Asegúrate de que la ruta sea correcta
 
-function WhatsappButton() {
-  const phoneNumber = '5491163569666'; // Reemplaza con tu número de teléfono
+const phoneNumber = '5491163569666'; // Reemplaza con tu número de teléfono
 
-  const handleClick = () => {
-    const url = `https://wa.me/${phoneNumber}`;
-    window.open(url, '_blank');
-  };
+const handleClick = () => {
+  const url = `https://wa.me/${phoneNumber}`;
+  window.open(url, '_blank');
+};
 
+const WhatsappButton = () => {
   return (
     <button className="whatsapp-button" onClick={handleClick}>
-      <img src="src/whatsapp-icon.png" alt="WhatsApp" className="whatsapp-icon" />
-      
+      <img src={whatsappIcon} alt="WhatsApp" className="whatsapp-icon" />
     </button>
   );
 }
